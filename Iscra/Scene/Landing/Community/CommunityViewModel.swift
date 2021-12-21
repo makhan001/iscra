2c348b993ef095f1b306a4a175d61dea6cadb313
deepakios-thoughtwin <90381993+deepakios-thoughtwin@users.noreply.github.com>
2021-12-21 18:56:44 +0530

    Working on community screen and its api

--- Iscra/Scene/Landing/Community/CommunityViewModel.swift
+++ Iscra/Scene/Landing/Community/CommunityViewModel.swift
@@ -10,22 +10,20 @@ import Foundation
 
 final class CommunityViewModel {
     
-    var myGroupList = [GroupHabit]()
+    var arrMyGroupList = [GroupHabit]()
+    var arrInvitaions = [Invitaion]()
+    
     let provider: CommunityServiceProvidable
     weak var view: CommunityViewRepresentable?
     var delegate: CommunityServiceProvierDelegate?
-    
-//    var pullToRefreshCtrl:UIRefreshControl!
-//    var isRefreshing = false
     
     init(provider: CommunityServiceProvidable) {
         self.provider = provider
         self.provider.delegate = self
     }
     
-     func fetchCommunityList() {
+    func fetchCommunityList() {
         self.provider.fetchCommunity(param: CommunityParams.FetchCommunity())
-       
     }
 }
 
@@ -36,14 +34,19 @@ extension CommunityViewModel: CommunityServiceProvierDelegate{
             if error != nil {
                 self.view?.onAction(.errorMessage(error?.responseData?.message ?? ERROR_MESSAGE))
             } else {
-                if let resp = response as? SuccessResponseModel, resp.code == 200, let groupList = resp.data?.groupHabits {
-                    let arrForGroup = groupList.filter({$0.habitType == "group_habit"})
-                    self.myGroupList = arrForGroup
-                    self.myGroupList.sort { Int($0.createdAt!) > Int($1.createdAt!) }
-                    print("arrForGroup.count is \(arrForGroup.count)")
-                    print("self.groupList is \(self.myGroupList.count)")
+                if let resp = response as? SuccessResponseModel, resp.code == 200, let groupList = resp.data?.groupHabits, let invitaionsList = resp.data?.invitaions{
+                    //                    let arrForGroup = groupList.filter({$0.habitType == "group_habit"})
+                    //                    self.myGroupList = arrForGroup
+                    self.arrMyGroupList = groupList
+                    self.arrMyGroupList.sort { Int($0.createdAt!) > Int($1.createdAt!) }
+                    print("self.groupList is \(self.arrMyGroupList.count)")
                     self.view?.onAction(.sucessMessage(resp.message ?? ""))
-                } else {
+                    
+                    self.arrInvitaions = invitaionsList
+                     self.arrInvitaions.sort { Int($0.createdAt!) > Int($1.createdAt!) }
+                    print("self.arrInvitaions is \(self.arrInvitaions.count)")
+                    self.view?.onAction(.sucessMessage(resp.message ?? ""))
+                }else {
                     self.view?.onAction(.sucessMessage((response as? SuccessResponseModel)?.message ?? ERROR_MESSAGE))
                 }
             }

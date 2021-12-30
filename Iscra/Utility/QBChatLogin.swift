374471fe220c200c8ae71541dd322820a873b44a
madhuritw <madhuri.thoughtwin@gmail.com>
2021-12-30 19:17:54 +0530

    fix chat navigation bar issue

--- Iscra/Utility/QBChatLogin.swift
+++ Iscra/Utility/QBChatLogin.swift
@@ -11,23 +11,8 @@ import UIKit
 class QBChatLogin {
     static let shared = QBChatLogin()
     
-        var dialogID: String? {
-            didSet {
-               // handlePush()
-            }
-        }
+    //Mark:- Signp user in chat
     func registerQBUser() {
-       
-//        let user = QBUUser()
-//        user.email = UserStore.userEmail
-//        user.login = UserStore.userEmail
-//        user.fullName = UserStore.userName
-//        user.password = AppConstant.defaultQBUserPassword
-//        QBRequest.signUp(user, successBlock: { response, user in
-//            print("UserSignUpInQuickBlox", user)
-//        }, errorBlock: { (response) in
-//            print("UserNOTSignUpInQuickBlox", response)
-//        })
         let user = QBUUser()
         user.login = UserStore.userEmail
         user.fullName = UserStore.userName
@@ -47,91 +32,41 @@ class QBChatLogin {
                     return
                 }
                 print("UserNOTSignUpInQuickBlox", response)
-             //   self?.handleError(response.error?.error, domain: ErrorDomain.signUp)
-        })
+             })
     }
     
-    //func loginQBUser(email:String) {
+    //Mark:- Login user in chat
     func loginQBUser(fullName: String, login: String, password: String = AppConstant.defaultQBUserPassword){
-//        QBRequest.logIn(withUserEmail: email, password: AppConstant.defaultQBUserPassword) { (response, user) in
-//            let current = Profile()
-//            do {
-//                let data = try NSKeyedArchiver.archivedData(withRootObject: user, requiringSecureCoding: false)
-//                let userDefaults = UserDefaults.standard
-//                userDefaults.set(data, forKey: UserProfileConstant.curentProfile)
-//            } catch {
-//                debugPrint("[Profile] Couldn't write file to UserDefaults")
-//            }
-//            print(current.fullName)
-//            self.connectToChat(user: user, userPassword:AppConstant.defaultQBUserPassword)
-//
-//        } errorBlock: { (response) in
-//            print("LoginChatNotSuccess",response)
-//        }
-        
-        QBRequest.logIn(withUserLogin: UserStore.userEmail ?? "",
+          QBRequest.logIn(withUserLogin: UserStore.userEmail ?? "",
                         password: AppConstant.defaultQBUserPassword,
                         successBlock: { [weak self] response, user in
                             guard let self = self else {
                                 return
                             }
-                            
                             user.password = AppConstant.defaultQBUserPassword
                             Profile.synchronize(user)
-                            
                             if user.fullName != fullName {
                                 print(user.fullName)
                                // self.updateFullName(fullName: fullName, login: login)
                             } else {
-                                self.connectToChat(user: user, userPassword: AppConstant.defaultQBUserPassword)
+                                self.connectToChat(user: user)
                                 print("user===>\(user)")
                             }
                             print("LoginChatSuccess",response)
                             
             }, errorBlock: { [weak self] response in
-               // self?.handleError(response.error?.error, domain: ErrorDomain.logIn)
-                if response.status == QBResponseStatusCode.unAuthorized {
-                    // Clean profile
+              if response.status == QBResponseStatusCode.unAuthorized {
                     Profile.clearProfile()
-                   // self?.defaultConfiguration()
                     print("LoginChatNotSuccess",response)
                 }
                
         })
         
     }
-    
-    private func connectToChat(user: QBUUser, userPassword:String) {
-//        if QBChat.instance.isConnected == true {
-//            //did Login action
-//            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .nanoseconds(Int(0.01))) {
-//                print("Connected")
-//            }
-//        } else {
-//            QBChat.instance.connect(withUserID: user.id,
-//                                    password: userPassword,
-//                                    completion: { [weak self] error in
-//                guard let _ = self else { return }
-//                if let error = error {
-//                    if error._code == QBResponseStatusCode.unAuthorized.rawValue {
-//                        Profile.clearProfile()
-//                    } else {
-//                        print("not Connected")
-//                    }
-//                } else {
-//                    //did Login action
-//                    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .nanoseconds(Int(0.01))) {
-//                        AppDelegate.shared.setRootController().self
-//                        print("mak")
-//                    }
-//                }
-//            })
-//        }
-        if QBChat.instance.isConnected == true {
-            //did Login action
-            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .seconds(1)) {
-                //AppDelegate.shared.rootController.goToDialogsScreen()
-                
+    //Mark:- Connect to chat
+    private func connectToChat(user: QBUUser) {
+       if QBChat.instance.isConnected == true {
+           DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .seconds(1)) {
             }
         } else {
             QBChat.instance.connect(withUserID: user.id,
@@ -144,93 +79,25 @@ class QBChatLogin {
                                                 Profile.clearProfile()
                                                 //self.defaultConfiguration()
                                             } else {
-                                               // self.showAlertView(LoginConstant.checkInternet, message: LoginConstant.checkInternetMessage)
-                                                //self.handleError(error, domain: ErrorDomain.logIn)
-//                                                print("not Connected")
                                             }
                                         } else {
-                                            //did Login action
-                                           // DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .nanoseconds(Int(0.01))) {
-                                               // AppDelegate.shared.rootViewController.goToDialogsScreen()
-                                           // self.goToDialogsScreen()
-                                            //self.showLoginScreen()
-                                              //  self.inputedUsername = nil
-                                        
-                                               // self.inputedLogin = nil
-                                           // }
                                         }
                                     })
-            
-        }
+            }
     }
-//    func goToDialogsScreen() {
-//        let storyboard = UIStoryboard(name: "Dialogs", bundle: nil)
-//        if let dialogs = storyboard.instantiateViewController(withIdentifier: "DialogsViewController") as? DialogsViewController {
-//            let dialogsScreen = DialogsNavigationController(rootViewController: dialogs)
-//            dialogsScreen.navigationBar.barTintColor = #colorLiteral(red: 0.2216441333, green: 0.4713830948, blue: 0.9869660735, alpha: 1)
-//            dialogsScreen.navigationBar.barStyle = .black
-//
-//            dialogsScreen.navigationBar.isTranslucent = true
-//            dialogsScreen.navigationBar.tintColor = .white
-//            dialogsScreen.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.red]
-//           // changeCurrentViewController(dialogsScreen)
-//                 //  handlePush()
-//            }
-//    }
-   
-//        private func changeCurrentViewController(_ newCurrentViewController: UIViewController) {
-//            addChild(newCurrentViewController)
-//            newCurrentViewController.view.frame = view.bounds
-//            view.addSubview(newCurrentViewController.view)
-//            newCurrentViewController.didMove(toParent: self)
-//            var current: UIViewController = newCurrentViewController
-//            if current == newCurrentViewController {
+//    private func updateFullName(fullName: String, login: String) {
+//        let updateUserParameter = QBUpdateUserParameters()
+//        updateUserParameter.fullName = fullName
+//        QBRequest.updateCurrentUser(updateUserParameter, successBlock: { [weak self] response, user in
+//            guard let self = self else {
 //                return
 //            }
-//            current.willMove(toParent: nil)
-//            current.view.removeFromSuperview()
-//            current.removeFromParent()
-//            current = newCurrentViewController
-//        }
+//            //self.infoText = LoginConstant.fullNameDidChange
+//            Profile.update(user)
+//            self.connectToChat(user: user, userPassword: AppConstant.defaultQBUserPassword)
 //
-//        private func handlePush() {
-//            let storyboardDia = UIStoryboard(name: "Dialogs", bundle: nil)
-//            let dialogsVC = storyboardDia.instantiateViewController(withIdentifier: "DialogsViewController") as? DialogsViewController
-//            let dialogsScreen = DialogsNavigationController(rootViewController: dialogsVC!)
-//            if let dialogsNavigationController = dialogsVC as? DialogsNavigationController, let dialogID = dialogID {
-//                if let dialog = ChatManager.instance.storage.dialog(withID: dialogID) {
-//                    // Autojoin to the group chat
-//                    if dialog.type != .private, dialog.isJoined() == false {
-//                        dialog.join(completionBlock: { error in
-//                            guard let error = error else {
-//                                return
-//                            }
-//                            debugPrint("[RootParentVC] dialog.join error: \(error.localizedDescription)")
-//                        })
-//                    }
-//                    dialogsNavigationController.popToRootViewController(animated: false)
-//                    (dialogsNavigationController.topViewController as? DialogsViewController)?.openChatWithDialogID(dialogID)
-//                    self.dialogID = nil
-//                }
-//            }
-//        }
-//        // MARK: - Handle errors
-//        private func handleError(_ error: Error?, domain: ErrorDomain) {
-//            guard let error = error else {
-//                return
-//            }
-//            var infoText = error.localizedDescription
-//            if error._code == NSURLErrorNotConnectedToInternet {
-//                infoText = LoginConstant.checkInternet
-//            }
-//        }
-//    func showLoginScreen() {
-//        let dialogsViewController = DialogsViewController()
-//        let navigationController = UINavigationController(rootViewController: dialogsViewController)
-//
-//        navigationController.pushViewController(dialogsViewController, animated: true)
-//        
-//        //changeCurrentViewController(DialogsVC)
+//            }, errorBlock: { [weak self] response in
+//                //self?.handleError(response.error?.error, domain: ErrorDomain.signUp)
+//        })
 //    }
-    
 }
